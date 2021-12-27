c48be82fca3fd2975b5d8f39553d675230fba7ff
vu21csen0100391 <95602850+vu21csen0100391@users.noreply.github.com>
2021-12-27 10:21:17 +0530

    Add files via upload

---
+++ capital letters.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    printf("Uppercase characters : \n");
+    for (ch = 'A'; ch <= 'Z'; ch++)
+    {
+        printf("%c ", ch);
+    }
+}
---
+++ upperlowercase.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Uppercase characters : \n");
+    for (ch = 'A'; ch <= 'Z'; ch++)
+    {
+        printf("%c ", ch);
+    }
+    printf("\nLowercase characters : \n");
+    for (ch = 'a'; ch <= 'z'; ch++)
+    {
+        printf("%c ", ch);
+    }
+}
+
+
